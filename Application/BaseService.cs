9ae63ffe2a2b544a293c4b1648b911a8bdc3986a
BlenchuRot <belen.aguirre.de.carcer.rojo@gmail.com>
2020-02-24 13:18:53 +0100

    reload

--- Application/BaseService.cs
+++ Application/BaseService.cs
@@ -3,6 +3,7 @@ using System;
 using Api.Application;
 
 namespace Api.Application
+
 {
     class BaseService : IDisposable
     {
--- Application/UserService.cs
+++ Application/UserService.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 using Api.RegistroUsuario;
 using Api.RegistroUsuariode;
 using API.Infraestructure;
@@ -16,40 +15,3 @@ namespace Api.Application
 
         public UserRegistered Register(UserRegistration userRegistration)
         {
-            var user = User.Create(userRegistration);
-            _context.User.Add(user);
-            _context.SaveChanges();
-            return UserRegistered.Create(user);
-=======
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using Api.Dominio;
-using Api.RegistroUsuariode;
-
-namespace Api.ControladorUsuario
-{
-    [ApiController]
-    [Route("[controller]")]
-    public class UserController : ControllerBase
-    {
-        private readonly ILogger<UserController> _logger;
-        private readonly IUserService _userService;
-        public UserController(ILogger<UserController> logger, IUserService userService)
-        {
-            _logger = logger;
-            _userService = userService;
-        }
-        [HttpPost]
-        public IActionResult Post([FromBody] UserRegistration userRegistration)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
-            UserRegistered userRegistered = _userService.Register(userRegistration);
-            // TODO: return Created
-            return Ok(userRegistered);
->>>>>>> 61e19685840607a8c7127871aa8380010e25b183
-        }
-    }
-}
