61e19685840607a8c7127871aa8380010e25b183
Antonio Peinado <60774350+AntonioPeinado@users.noreply.github.com>
2020-02-24 13:12:08 +0100

    controller

---
+++ Application/UserService.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Api.Dominio;
+using Api.RegistroUsuariode;
+
+namespace Api.ControladorUsuario
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : ControllerBase
+    {
+        private readonly ILogger<UserController> _logger;
+        private readonly IUserService _userService;
+        public UserController(ILogger<UserController> logger, IUserService userService)
+        {
+            _logger = logger;
+            _userService = userService;
+        }
+        [HttpPost]
+        public IActionResult Post([FromBody] UserRegistration userRegistration)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            UserRegistered userRegistered = _userService.Register(userRegistration);
+            // TODO: return Created
+            return Ok(userRegistered);
+        }
+    }
+}
---
+++ Controller/UserController.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using API.Application;
+using Api.RegistroUsuariode;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : ControllerBase
+    {
+        private readonly ILogger<UserController> _logger;
+        private readonly IUserService _userService;
+        public UserController(ILogger<UserController> logger, IUserService userService)
+        {
+            _logger = logger;
+            _userService = userService;
+        }
+        [HttpPost]
+        public IActionResult Post([FromBody] UserRegistration userRegistration)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            UserRegistered userRegistered = _userService.Register(userRegistration);
+            // TODO: return Created
+            return Ok(userRegistered);
+        }
+    }
+}
