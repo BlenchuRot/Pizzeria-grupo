b36e84c740d6adf9749af2b4a750966eb700cfef
BlenchuRot <belen.aguirre.de.carcer.rojo@gmail.com>
2020-03-03 10:15:06 +0100

    IngredientService

--- Application/IngredienteService.cs
+++ Application/IngredienteService.cs
@@ -1,23 +1,29 @@
+using System;
 using Pizzeria.DTO;
 using Pizzeria.Infraestructure;
 using Pizzeria.Dominio;
 namespace Pizzeria.Application
 {
-   public class IngredientService : Ingredient
+    public class IngredientService : Ingredient
     {
-         private readonly PizzeriaContext _context;
+        private readonly PizzeriaContext _context;
 
         public IngredientService(PizzeriaContext context)
-            {
-                _context = context;
+        {
+            _context = context;
 
-            }
-        public void Create (CreateIngredientDTO createIngredientDTO){
-           var ingredient = new Ingredient();
-           _context.Ingredient.Add(ingredient);
-           _context.SaveChanges();
-           _context.Dispose();
+        }
+        public void Create(CreateIngredientDTO createIngredientDTO)
+        {
+            var ingredient = new Ingredient();
+            _context.Ingredient.Add(ingredient);
+            _context.SaveChanges();
+            _context.Dispose();
 
+        }
+        public Ingredient GetById(Guid id)
+        {
+            return _context.Ingredient.Find(id);
         }
 
     }
