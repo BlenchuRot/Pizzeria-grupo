be9746f07c8e26842364596caac960a68b00b50f
BlenchuRot <belen.aguirre.de.carcer.rojo@gmail.com>
2020-03-04 13:57:23 +0100

    peaso IngredientService

--- Application/Ingredient/IngredientService.cs
+++ Application/Ingredient/IngredientService.cs
@@ -3,7 +3,6 @@ using Pizzeria.DTO;
 using Pizzeria.Infraestructure;
 using System.Collections.Generic;
 using System.Linq;
-using Pizzeria.Dominio;
 namespace Pizzeria.Application
 {
     public class IngredientService : IIngredientService
