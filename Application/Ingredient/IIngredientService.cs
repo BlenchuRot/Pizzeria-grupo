9226e9703bfc45b5ae40869d7e4e4abf125ee1e3
BlenchuRot <belen.aguirre.de.carcer.rojo@gmail.com>
2020-03-04 12:27:01 +0100

    fix

--- Application/Ingredient/IIngredientService.cs
+++ Application/Ingredient/IIngredientService.cs
@@ -3,12 +3,8 @@
 namespace Pizzeria.Application
 {
     public interface IIngredientService
-<<<<<<< HEAD
-    {        void Read(ReadIngredientDTO ingredientDTO);
-=======
     {
         void Read(ReadIngredientDTO ingredientDTO);
->>>>>>> 4c0785f5e1ccbca30f5c9dc7bc741ef8455e7329
 
 
     }
