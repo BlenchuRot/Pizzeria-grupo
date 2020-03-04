a1dd6468103c3722e1a0aa649565d39651d17239
Antonio Peinado <60774350+AntonioPeinado@users.noreply.github.com>
2020-03-04 09:51:52 +0100

    ingredientcontroller21

--- Application/Comment/ICommentService.cs
+++ Application/Comment/ICommentService.cs
@@ -6,13 +6,9 @@ namespace Pizzeria.Application
     public interface ICommentService
     {
         ReadCommentDTO Create(CreateCommentDTO createCommentDTO);
-<<<<<<< HEAD
-        CreateCommentDTO Register(CreateCommentDTO commentRegistration);
-=======
 
-        void Read(CreateCommentDTO CommentDetailDTO);
-        CreateCommentDTO Register(CreateCommentDTO commentRegistration);     
->>>>>>> c8a251bfc568a159c84769a079127e8e92d22287
+        void Read(CreateCommentDTO commentDetailDTO);
+        CreateCommentDTO Register(CreateCommentDTO commentRegistration);
 
     }
 } 
