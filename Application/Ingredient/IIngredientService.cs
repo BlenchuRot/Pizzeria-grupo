7fd9ebf98b671dc5976f663ed25a19745fab1f93
Antonio Peinado <60774350+AntonioPeinado@users.noreply.github.com>
2020-03-04 09:46:53 +0100

    ingredientcontroller21

--- Application/Ingredient/IIngredientService.cs
+++ Application/Ingredient/IIngredientService.cs
@@ -7,7 +7,7 @@ namespace Pizzeria.Application
         void Create(CreateIngredientDTO createIngredientDTO);
         void Read(IngredientRegisterDTO ingredientDetailDTO);
         IngredientRegisterDTO Register(IngredientRegisterDTO ingredientRegisterDTO);
-
+        CreateIngredientDTO Create(IIngredientService createIngredientDTO);
     }
 
 }
--- DTO/Pizza/PizzasRegistradas.cs
+++ DTO/Pizza/PizzasRegistradas.cs
@@ -28,10 +28,6 @@ namespace Pizzeria.DTO
         private double Price { get; set; }
 
 
-        internal static PizzaRegistered Create(Object id)
-
-        {
-            throw new NotImplementedException();
-        }
+       
     }
 }
