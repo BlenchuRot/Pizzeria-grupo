bd449dbfca7746b1dad682502c105b8fa985aab4
LuciaHub <luciaprogramacion11@gmail.com>
2020-03-05 10:41:47 +0100

    fix controller OK

--- Controller/IngredientController.cs
+++ Controller/IngredientController.cs
@@ -12,37 +12,14 @@ namespace Pizzeria.Controllers
     public class IngredientController : ControllerBase
     {
         private readonly IIngredientService _ingredientService;
-<<<<<<< HEAD
         public IngredientController(IIngredientService ingredientService)
-=======
-
-
-
-        public IngredientController(ILogger<IngredientController> logger, IIngredientService ingredientService)
->>>>>>> be10550a5e64f1179615617a1b3cab745f2f054e
         {
             _ingredientService = ingredientService;
         }
         [HttpGet] 
         public IActionResult Get()
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
-<<<<<<< HEAD
-            
-            return StatusCode(200, _ingredientService.ReadAll());
-=======
-            //Generar un .log que nos devuelva "Hola".
-            _logger.LogCritical("Hola");
-
-            ReadIngredientDTO readIngredientDTO = _ingredientService.ReadAll();
-            //return Created;
-            return StatusCode(201, readIngredientDTO);
->>>>>>> be10550a5e64f1179615617a1b3cab745f2f054e
-
-           
+        {   
+            return StatusCode(200, _ingredientService.ReadAll());  
         }
     }
 }
