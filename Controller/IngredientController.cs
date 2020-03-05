d72d31c14027a01f0fc50e2301546b74090b8faf
LuciaHub <luciaprogramacion11@gmail.com>
2020-03-05 10:26:33 +0100

    fix controller OK

--- Controller/IngredientController.cs
+++ Controller/IngredientController.cs
@@ -3,6 +3,7 @@ using Microsoft.Extensions.Logging;
 using Pizzeria.Application;
 using System;
 using Pizzeria.Dominio;
+using Pizzeria.DTO;
 
 namespace Pizzeria.Controllers
 {
@@ -10,26 +11,20 @@ namespace Pizzeria.Controllers
     [Route("[controller]")]
     public class IngredientController : ControllerBase
     {
-        private readonly ILogger<IngredientController> _logger;
         private readonly IIngredientService _ingredientService;
-        public IngredientController(ILogger<IngredientController> logger, IIngredientService ingredientService)
+        public IngredientController(IIngredientService ingredientService)
         {
-            _logger = logger;
             _ingredientService = ingredientService;
         }
-        [HttpPost]
-        public IActionResult Post([FromBody] IIngredientService ReadIngredientDTO)
+        [HttpGet] 
+        public IActionResult Get()
         {
             if (!ModelState.IsValid)
             {
                 return BadRequest(ModelState);
             }
-            //Generar un .log que nos devuelva "Hola".
-            _logger.LogCritical("Hola");
-
-            ReadIngredientDTO() readIngredientDTO = _ingredientService.ReadAll();
-            //return Created;
-            return StatusCode(201, readIngredientDTO);
+            
+            return StatusCode(200, _ingredientService.ReadAll());
 
            
         }
